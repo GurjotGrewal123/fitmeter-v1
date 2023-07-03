5609fe2b09de92b4b095af08a9a31a6ec00d81fc
GurjotGrewal123 <gurjot_grewal@outlook.com>
2023-07-02 23:43:34 -0400

    fixed navigation using Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import '../styles/Header.css';
 import { useState } from 'react'
 import Cart from './Cart.js'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
@@ -9,9 +10,9 @@ const Header = () => {
             <a class="navbar-logo" href="/">Logo</a>
             <div class="navbar-pages">
                 <ul class="navbar-links">
-                    <li><a href="/fitmeter-v1/Progress">Progress</a></li>
-                    <li><a href="/fitmeter-v1/Nutrition">Nutrition</a></li>
-                    <li><a href="/fitmeter-v1/Workout">Workout</a></li>
+                    <li><Link to="/fitmeter-v1/Progress">Progress</Link></li>
+                    <li><Link to="/fitmeter-v1/Nutrition">Nutrition</Link></li>
+                    <li><Link to="/fitmeter-v1/Workout">Workout</Link></li>
                 </ul>
             </div>
         </div>
